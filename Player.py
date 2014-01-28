7c88ab6c90b0019bd6350ccced6bb2a651b054d6
Max G <mgx2@inbox.lv>
2014-01-29 00:12:32 +0100

    new methods move_mouse and changeSpeed

--- Player.py
+++ Player.py
@@ -11,3 +11,24 @@ class Player(GenObj):
         self.img = img
         self.rect = rect
         self.bound = boundries
+
+        self.speed = [0,0]
+
+    def move_mouse(self, new_coord):
+        self.rect.center = new_coord
+
+    def changeSpeed(self, new_speed):
+        tmpx = self.speed[0]
+        tmpy = self.speed[1]
+
+        tmpx += new_speed[0]
+        if (abs(tmpx) > 2):
+            pass
+        else:
+            self.speed[0] = tmpx
+
+        tmpy += new_speed[1]
+        if (abs(tmpy) > 2):
+            pass
+        else:
+            self.speed[1] = tmpy
