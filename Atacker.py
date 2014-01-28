185bf1989b5915cfe30453bfaed25acb1d8c25d9
Max G <mgx2@inbox.lv>
2014-01-29 00:11:58 +0100

    checkBound method returned

--- Atacker.py
+++ Atacker.py
@@ -17,3 +17,14 @@ class Atacker(GenObj):
         self.img_r = img_r
         self.rect = rect
         self.bound = boundries
+
+
+    def checkBound(self):
+        if self.rect.left < 0:
+            self.speed[0] = self.speed[0] * -1
+            self.img = self.img_r
+        if self.rect.right > self.bound[0]:
+            self.speed[0] = self.speed[0] * -1
+            self.img = self.img_l
+        if self.rect.top < 0 or self.rect.bottom > self.bound[1]:
+            self.speed[1] = self.speed[1] * -1   
