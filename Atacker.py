067454dcf0a7848756426f4344ed9cade2eb1f97
Max G <mgx2@inbox.lv>
2014-01-28 23:35:21 +0100

    Inherit methods from general class

--- Atacker.py
+++ Atacker.py
@@ -1,7 +1,8 @@
-class Atacker():
+from GenObj import *
+
+class Atacker(GenObj):
     """Basic enemy class"""
-    __Speed = []
-    __HP = 0
+
 
     # boundries (w,h)
     def __init__(self, name, speed, hp, img_l, img_r, rect, boundries):
@@ -16,35 +17,3 @@ class Atacker():
         self.img_r = img_r
         self.rect = rect
         self.bound = boundries
-
-    def stop(self):
-        self.speed = [0,0]
-
-    def start(self):
-        self.speed = self.__Speed
-
-    def fire(self):
-        pass
-
-    def hit(self, damage):
-        self.hp -= damage
-        if (self.hp < 1):
-            self.stop()
-
-    def checkBound(self):
-      #  self.rect.move(self.speed)
-      #  print "Checking speed "+ str(self.speed)
-        if self.rect.left < 0:
-            self.speed[0] = self.speed[0] * -1
-            self.img = self.img_r
-        if self.rect.right > self.bound[0]:
-            self.speed[0] = self.speed[0] * -1
-            self.img = self.img_l
-           # print "Vertikalnij BUM"
-        if self.rect.top < 0 or self.rect.bottom > self.bound[1]:
-            self.speed[1] = self.speed[1] * -1      
-#            print "Gorizontalnij BUM"
-
-
-    def getHP(self):
-        return self.hp
