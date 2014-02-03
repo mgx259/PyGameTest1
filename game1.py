56426bd77d203d8504ea0280c18d1849f6cd4bfd
Max G <mgx2@inbox.lv>
2014-02-03 22:04:02 +0100

    Player is an object; using lists for game objects; placing objects randomly; final screen

--- game1.py
+++ game1.py
@@ -1,5 +1,5 @@
 import sys, pygame
-import copy
+#import copy
 
 from pygame.locals import *
 from lib_game1 import *
@@ -8,21 +8,22 @@ from Player import *
 from Weapon import *
 
 
-### VARIABLES
-ver = 'v.0.01'
+### CONSTANTS
+cVer = 'v.0.01'
 cFPS = 30
-cAICooldown = 50
+cAliens = 4
+cAsteroids = 3
+
+cPlayerBeamSpd = 8
+cPlayerBeamDmg = 2
+cAlienBeamSpd  = -15
+cAlienBeamDmg  = 1
 
 
-spdCnt = cAICooldown
-
-
-# msg = 'Hi there!!!!'
 msgGO = 'Game Over :('
 msgWIN = 'You Win :)'
 msgPlrHP = ''
 finalMsg = ''
-
 
 mouse_x, mouse_y = 0, 0
 
@@ -31,24 +32,15 @@ atackersList = []
 deadList     = []
 allObjList   = []
 
-#showBeam   = False
-#showLaser  = False
-#showDead   = False
-#showDead2 = False
-gameOver   = False
-gameWon   = False
-
-
 
 ### PYGAME INIT
-
 pygame.init()
 
 fpsClock = pygame.time.Clock()
 
 size = width, height = 1120, 700    #bg.get_size() # 640, 480
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Space Invaiders '+ver)
+pygame.display.set_caption('Space Invaiders '+cVer)
 
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255,255,255)
@@ -58,8 +50,9 @@ blue  = pygame.Color(0, 0, 255)
 
 
 ### LOADING RESOURCES
-
 bg, bgRect           = loadImg('Space-2.jpg', 'Backgrounds')
+screen_bound = (bgRect.width, bgRect.height)
+
 l_aln1, aln1Rect     = loadImg('alienspaceship_left.png', 'Spaceships')
 r_aln1, r_aln1Rect   = loadImg('alienspaceship_right.png', 'Spaceships')
 d_aln, d_alnRect     = loadImg('alienspaceship_left_dead.png', 'Spaceships')
@@ -72,7 +65,6 @@ alnPrt4, alnPrt4Rect   = loadImg('wing_r2.png', 'Spaceships')
 station, stationRect = loadImg('Spacestation.png')
 astr, astrRect = loadImg('Asteroid_small_1.png')
 
-
 beam1, beam1Rect     = loadImg('beam1_yellow_2.png', 'Weaporns')
 laser1, laser1Rect   = loadImg('laser1_red.png', 'Weaporns')
 
@@ -82,56 +74,57 @@ sndLaser2 = loadSnd('laser-02.wav')
 powerup   = loadSnd('Power-Up.wav')
 
 
-fntObj  = loadFont('Anita semi square.ttf', 12)
+fntObj    = loadFont('Anita semi square.ttf', 12)
 fntGMObj  = loadFont('Anita semi square.ttf', 48)
-
-### SETTING COORDINATES
-
-aln1Rect.x = bgRect.width - aln1Rect.width - 1
-aln1Rect.y = aln1Rect.height + 1
-
-
-aln2Rect   = aln1Rect.copy() 
-aln2Rect.y = bgRect.height - aln2Rect.height - 1
-
-aln3Rect   = aln1Rect.copy() 
-aln3Rect.y = bgRect.height - aln3Rect.height - 1
-
-
-
-### POPULATING ATACKERS LIST
-
-screen_bound = (bgRect.width, bgRect.height)
-
-a1 = Atacker('KV1', 10, l_aln1, r_aln1, aln1Rect, screen_bound)
-a2 = Atacker('KV2', 10, l_aln1, r_aln1, aln2Rect, screen_bound)
-a3 = Atacker('KV3', 10, l_aln1, r_aln1, aln3Rect, screen_bound)
-
-atackersList.append(a1)
-atackersList.append(a2)
-atackersList.append(a3)
 
 
 ### CREATING PLAYER OBJECT
-
 plr = Player('MiR', 15, station, stationRect, screen_bound)
-
 plr.rect.x = plr.rect.width + 5
 plr.rect.y = (bgRect.height / 2) - (plr.rect.height / 2)
 
+allObjList.append(plr)
 
 
+# placementRetries = 20
+# colisionDetected = True
 
-ast1 = GenObj('Asteroid', 50, astr, astrRect, screen_bound)
-ast1.setRndPosition()
-# TODO: check if not colliding with other object
+### POPULATING ATACKERS LIST
+for ast_i in range(cAliens):
+    a1 = Atacker('Alien_'+str(ast_i), 10, l_aln1, aln1Rect, screen_bound)
 
-deadList.append(ast1)
+    colisionDetected = True
+    placementRetries = 10
+    while colisionDetected or placementRetries > 0:
+        a1.setRndPosition(False, True)
+        colisionDetected = checkCollision(a1.rect, allObjList)
+        placementRetries -= 1
+
+    if not colisionDetected:
+        allObjList.append(a1)
+        atackersList.append(a1)
+    else:
+        print "Cant plase {} after {} attempts".format(a1.name, 10)
 
 
-# allObjList.extend(plr)
-# allObjList.extend(atackersList)
-# allObjList.extend(deadList)
+### POPULATING ASTEROIDS
+for ast_i in range(cAsteroids):
+    ast1 = GenObj('Asteroid_'+str(ast_i), 50, astr, astrRect, screen_bound)
+
+    colisionDetected = True
+    placementRetries = 10
+    # while placementRetries > 0:
+    while colisionDetected or placementRetries > 0:
+        ast1.setRndPosition()
+        colisionDetected = checkCollision(ast1.rect, allObjList)
+        placementRetries -= 1
+
+    if not colisionDetected:
+        allObjList.append(ast1)
+        deadList.append(ast1)
+    else:
+        print "Cant plase {} after {} attempts".format(a1.name, 10)
+
 
 
 ########################
@@ -149,13 +142,12 @@ while len(finalMsg) == 0:
         elif event.type == MOUSEBUTTONUP:
           #  beam1_x, beam1_y = event.pos
             beamsList.append(Weapon('beam_'+ str(len(beamsList)), 
-                                     2, 5, 
+                                     cPlayerBeamDmg, cPlayerBeamSpd, 
                                      plr.rect.x + plr.rect.width + beam1Rect.width +1, 
                                      plr.rect.y + (plr.rect.height / 2),
                                      beam1, beam1Rect, sndLaser1)
             )
-
-            print "Beam List size: {}".format(len(beamsList))
+    #        print "Beam List size: {}".format(len(beamsList))
             sndLaser1.play()
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
@@ -175,83 +167,49 @@ while len(finalMsg) == 0:
 
     screen.blit(bg, bgRect)
 
+  #  spdCnt -= 1
 
 
-
-
-### Show dead ships
-    for dd in deadList:
-        screen.blit(dd.img, dd.rect)
-
-
-#    if showDead :
-#        screen.blit(d_aln, d_alnRect)
-
-    # if showDead2 :
-    #     screen.blit(kv1dead, kv2deadRect)
-
-######
-
-
-    spdCnt -= 1
-
-### Processing Atackers
-    for at in atackersList:
+### Processing all Objects
+    for at in allObjList:
         decision = None
-        at.rect = at.rect.move(at.speed)
-        at.checkBound()
+        at.moveit()
+
+
+
         screen.blit(at.img, at.rect)
 
-        msgPlrHP = str(at.getHP())
-        msgScreenObj = fntObj.render(msgPlrHP, False, red)
-        msgRect = msgScreenObj.get_rect()
-        msgRect.topleft = (at.rect.x + (at.rect.width/2 - msgRect.width/2), at.rect.y - (msgRect.height + 2))
-        screen.blit(msgScreenObj, msgRect)         
+        # print object HP if it's alive
+        if at.getHP() > 0:
+            msgPlrHP = str(at.getHP())
+            msgScreenObj = fntObj.render(msgPlrHP, False, red)
+            msgRect = msgScreenObj.get_rect()
+            msgRect.topleft = (at.rect.x + (at.rect.width/2 - msgRect.width/2), at.rect.y - (msgRect.height + 2))
+            screen.blit(msgScreenObj, msgRect)         
 
         # make some AI decision
-        if spdCnt < 0:
-            decision = at.ai_decision()
+        decision = at.ai_decision()
 
-            if decision['fire']:
-                laser1Rect.x = at.rect.centerx    
-                laser1Rect.y = at.rect.y + (at.rect.height / 2) 
-                beamsList.append(Weapon('laser_'+ str(len(beamsList)), 
-                                         1, -7, 
-                                         at.rect.x - (laser1Rect.width + 2), 
-                                         at.rect.y + (at.rect.height / 2),
-                                         laser1, laser1Rect, sndLaser2)
-                )
-              
-                sndLaser2.play() 
+        if 'fire' in decision and decision['fire']:
+            laser1Rect.x = at.rect.centerx    
+            laser1Rect.y = at.rect.y + (at.rect.height / 2) 
+            beamsList.append(Weapon('laser_'+ str(len(beamsList)), 
+                                     cAlienBeamDmg, cAlienBeamSpd, 
+                                     at.rect.x - (laser1Rect.width + 2), 
+                                     at.rect.y + (at.rect.height / 2),
+                                     laser1, laser1Rect, sndLaser2)
+            )
+          
+            sndLaser2.play() 
 
-            if decision['direction_change']:
-                powerup.play()
-
-    # reseting speed change counter
-    if spdCnt < 0:
-        spdCnt = cAICooldown
-
-
-### Processing Player
-    plr.rect = plr.rect.move(plr.speed)
-    plr.checkBound()
-    screen.blit(plr.img, plr.rect)
-
-    # printing Player HP
-    msgPlrHP = str(plr.getHP())
-    msgScreenObj = fntObj.render(msgPlrHP, False, red)
-    msgRect = msgScreenObj.get_rect()
-    msgRect.topleft = (plr.rect.x + (plr.rect.width/2 - msgRect.width/2), plr.rect.y - (msgRect.height + 2))
-    screen.blit(msgScreenObj, msgRect)    
+        if 'direction_change' in decision and decision['direction_change']:
+            powerup.play()
 
 
 ### Processing BEAMS
     for bm in beamsList:
         targetIdx = 0
         targetObj = None
-
-  #      print "BEAM: "+ bm.name + "  xy: "+ str(bm.pos) + " spd: "+str(bm.speed)
-  #      print "rect: "+ str(bm.rect.left) + " - "+ str(bm.rect.right) +" - "+ str(bm.rect.top) + " - "+ str(bm.rect.bottom)
 
         bm.rect = bm.rect.move(bm.speed)
         screen.blit(bm.img, bm.rect)
@@ -263,12 +221,11 @@ while len(finalMsg) == 0:
             beamsList.remove(bm)
             continue
 
-        ### hit alien
+        ### hit object
         targetIdx = bm.rect.collidelist(atackersList)
         if targetIdx > -1:
-            print "1 IDX: {}".format(targetIdx)
             targetObj = atackersList[targetIdx]
-            print "Popal v "+ targetObj.name + " HP left: "+ str(targetObj.getHP())
+            print "Popal v {}; HP left: {} isAlive: {}".format(targetObj.name, targetObj.getHP(), targetObj.isAlive())
             targetObj.hit(bm.damage)
             beamsList.remove(bm)
             sndAlien1.play()
@@ -281,7 +238,6 @@ while len(finalMsg) == 0:
         ### hit dead object 
         targetIdx = bm.rect.collidelist(deadList)
         if targetIdx > -1:
-            print "2 IDX: {}".format(targetIdx)
             targetObj = deadList[targetIdx]
             print "Popal v "+ targetObj.name + " HP left: "+ str(targetObj.getHP())
             targetObj.hit(bm.damage)
@@ -299,6 +255,15 @@ while len(finalMsg) == 0:
             beamsList.remove(bm)
             sndAlien1.play()
             continue
+
+    ### process colisions
+    for obj in allObjList:
+        allObjList.remove(obj)
+        targetIdx = obj.rect.collidelist(allObjList)
+        if targetIdx > -1:
+            obj.moveBack()
+            allObjList[targetIdx].moveBack()
+        allObjList.append(obj)
 
     ### player lost
     if plr.getHP() < 1:
@@ -322,6 +287,21 @@ while True:
             sys.exit()
 
     screen.blit(bg, bgRect)
+
+
+    for at in allObjList:
+        decision = None
+        screen.blit(at.img, at.rect)
+
+        # print object HP if it's alive
+        if at.getHP() > 0:
+            msgPlrHP = str(at.getHP())
+            msgScreenObj = fntObj.render(msgPlrHP, False, red)
+            msgRect = msgScreenObj.get_rect()
+            msgRect.topleft = (at.rect.x + (at.rect.width/2 - msgRect.width/2), at.rect.y - (msgRect.height + 2))
+            screen.blit(msgScreenObj, msgRect)  
+
+
 
     msgScreenObj = fntGMObj.render(finalMsg, False, red)
     msgRect = msgScreenObj.get_rect()
